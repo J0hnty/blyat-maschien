8da0edbdc489b1ebcb0387ba48bc96fdda07a32c
Johnty Van Delden <302052166@student.rocmondriaan.nl>
2021-04-20 23:38:38 +0200

    alles af behalven dot

--- Js/main.js
+++ Js/main.js
@@ -1,10 +1,11 @@
 let display='0';
-let operator=null;
-let firstValue=null;
-let secondValue=null;
+let operator='';
+let firstValue='';
+let secondValue='';
+let number='';
+let total=0;
 
-myButton=document.querySelector('input');
-
+let myButton=document.querySelector('input');
 let bodyElement=document.querySelector('body');
 let jumbotronElement=document.querySelector('.jumbotron');
 let displayElement=document.querySelector('.display');
@@ -13,19 +14,10 @@ let buttonsElement=document.querySelector('.buttons');
 myButton.addEventListener("click", toggleDarkMode);
 buttonsElement.addEventListener('click', handleClick);
 
-function toggleDarkMode() {
-
-    bodyElement.classList.toggle("dark-body");
-    jumbotronElement.classList.toggle("dark-blue-background");
-    displayElement.classList.toggle("dark-blue-background");
-    
-
-}
-
 function handleClick(event) {
 
     const element=event.target;
-
+    
     switch(element.className)
     {
         case 'btn-number':
@@ -40,13 +32,16 @@ function handleClick(event) {
         case 'btn-dot':
             dot();
             break
+        case 'btn-equal':
+            equals();
+            break;
     }
     
 }
 
 function handleNumber(el) {
 
-    let number=el.innerHTML;
+    number=el.innerHTML;
 
     if(display==='0') {
         display=number;
@@ -60,38 +55,47 @@ function handleNumber(el) {
 }
 
 function handleOperator(el) {
+    operator=el.innerHTML;
     firstValue=display;
-    operator=el;
+    display='0';
+
+    showDisplay();
+}
+
+function equals() {
+    secondValue=display;
+    display='0'
+    console.log(firstValue, secondValue);
+    let one = parseFloat(firstValue);
+    let two = parseFloat(secondValue);
+    console.log(`getal1: ${one}, getal2: ${two}`);
     console.log(operator);
-    switch(operator)
-    {
-        case '+':
-            
-            break;
-        case '-':
-
-            break;
-        case '*':
-
-            break;
-        case '/':
-
-            break;
-
+    if (operator === '+') {
+        total = one+two;
+    }else if (operator === '-') {
+        total = one-two;
+    }else if (operator === 'x') {
+        total = one*two;
+    }else if (operator === '/') {
+        total = one/two;
     }
+    display=total;
+    console.log(total);
+    showDisplay();
+}
 
+function clear() {
+    displayElement.innerHTML = '0';
+    display = '0';
+    console.log('display cleared');
 }
 
 function dot() {
     if (!displayElement.innerHTML.includes('.')) {
         displayElement.innerHTML += '.';
+        display + '.';
     }
 
-}
-
-function clear() {
-    displayElement.innerHTML = '0';
-    display = '';
 }
 
 function handleEindSom() {
@@ -106,6 +110,15 @@ function handleEindSom() {
     }
 }
 
+function toggleDarkMode() {
+
+    bodyElement.classList.toggle("dark-body");
+    jumbotronElement.classList.toggle("dark-blue-background");
+    displayElement.classList.toggle("dark-blue-background");
+    
+}
+
 function showDisplay() {
     displayElement.innerHTML=display;
 }
+
